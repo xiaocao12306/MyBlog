e148468757e826c8d74b5c17e41a14b1bbb70b69
小曹 <3027478654@qq.com>
2022-02-21 13:52:24 +0800

    FIXED：更改评论区设定

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -209,8 +209,8 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         clientID: 'a6e1355287947096b88b',
         clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
         repo: 'blog-gitalk-comment', // GitHub 仓库
-        owner: 'xugaoyi', // GitHub仓库所有者
-        admin: ['xugaoyi'], // 对仓库有写权限的人
+        owner: 'xiaocao12306', // GitHub仓库所有者
+        admin: ['xiaocao'], // 对仓库有写权限的人
         // distractionFreeMode: true,
         pagerDirection: 'last', // 'first'正序 | 'last'倒序
         id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
